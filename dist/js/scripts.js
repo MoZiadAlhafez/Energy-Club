400b27ff23218c36a97012c5124e3260e6e3b8a1
ziadalhafez <moziadalhafez@gmail.com>
2019-10-13 11:30:09 +0200

    Upload Projec

---
+++ dist/js/scripts.js
@@ -0,0 +1,2 @@
+var scroll=new SmoothScroll('a[data-target="inpage"]',{speed:1e3}),swiper=new Swiper(".products .swiper-container",{slidesPerView:4,spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0}});swiper=new Swiper(".trainers .swiper-container",{slidesPerView:5,spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=scripts.js.map
---
+++ project/js/Navbar.js
@@ -0,0 +1,33 @@
+// $("body").niceScroll({
+//   cursorcolor:"#4c368b",
+//   cursorwidth:"10px",
+//   background:"#f18c00",
+//   cursorborder:'none'
+// });
+
+var scroll = new SmoothScroll('a[data-target="inpage"]', {
+    speed: 1000,
+});
+
+
+
+
+
+var swiper = new Swiper('.products .swiper-container', {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+
+
+    var swiper = new Swiper('.trainers .swiper-container', {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
